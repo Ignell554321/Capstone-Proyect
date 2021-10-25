be77ce5f53adc5eaa66587fa0e890844fb8baf83
miguel <miguelguevaraalejandro@gmail.com>
2021-10-25 08:12:14 -0500

    25-10-2021

--- Services/AvatexApi-main/src/main/java/com/example/Avatex_api/controller/InventarioRestController.java
+++ Services/AvatexApi-main/src/main/java/com/example/Avatex_api/controller/InventarioRestController.java
@@ -1,16 +1,18 @@
 package com.example.Avatex_api.controller;
 
 import com.example.Avatex_api.dto.common.AnioMesRequestDto;
-import com.example.Avatex_api.dto.common.BuscaXFechaRequestDto;
 import com.example.Avatex_api.service.IInventarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@CrossOrigin(origins = "*")
 @RequestMapping("/Inventario")
 @RestController
 public class InventarioRestController {
